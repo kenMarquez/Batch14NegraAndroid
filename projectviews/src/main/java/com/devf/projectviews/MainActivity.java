97be32d3822ecfa5d0b444ae4be555567e19a3af
Ken Marquez <darius_dam@hotmail.com>
2017-04-20 19:12:19 -0500

    Add layouts class

---
+++ projectviews/src/main/java/com/devf/projectviews/MainActivity.java
@@ -0,0 +1,17 @@
+package com.devf.projectviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    Switch swich;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edittext);
+
+    }
+}
